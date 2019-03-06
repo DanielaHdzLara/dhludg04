581e5ebf014e8b248b3fa4f1e2404bd9c4304dba
DanielaHdzLara <43969822+DanielaHdzLara@users.noreply.github.com>
2019-03-06 12:45:13 -0600

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 // Make connection
-//var socket = io.connect(window.location.hostname);
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect(window.location.hostname);
+//var socket = io.connect('http://localhost:4000');
 
 // Query DOM
 var message = document.getElementById('message'),
