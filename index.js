bdfcd38d59836156f7b6fa43c6b7ecd3d7fe49bd
DanielaHdzLara <43969822+DanielaHdzLara@users.noreply.github.com>
2019-03-06 11:18:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var socket = require('socket.io');
 var port = process.env.PORT || 5000;
 
-//Heroku no permite el uso de WebSockets, entonces se configuro polling.
-//socket.configure (function() {
-//    socket.set("transports", ["xhr-polling"]);
-//    socket.set("polling duration", 10);
-//});
+Heroku no permite el uso de WebSockets, entonces se configuro polling.
+socket.configure (function() {
+    socket.set("transports", ["xhr-polling"]);
+    socket.set("polling duration", 10);
+});
 
 // App setup
 var app = express();
